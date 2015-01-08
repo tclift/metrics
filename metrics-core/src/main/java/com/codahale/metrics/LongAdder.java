99419a0a12542dbeb9f353da2915429001568db8
Tom Clift <tom.clift@papercut.com>
2015-01-08 14:48:30 +1100

    App Engine support: remove use of `sun.misc.Unsafe`. This involved removing Striped64 (which requires `Unsafe`) and replacing method implementations in `LongAdder` with `AtomicLong` equivalents. This effectively reverts ffeed35 while keeping API compatibility on `LongAdder` to avoid widespread changes.
    #refs dropwizard/metrics#539

--- metrics-core/src/main/java/com/codahale/metrics/LongAdder.java
+++ metrics-core/src/main/java/com/codahale/metrics/LongAdder.java
@@ -1,197 +1,74 @@
 /*
- * Written by Doug Lea with assistance from members of JCP JSR-166
- * Expert Group and released to the public domain, as explained at
- * http://creativecommons.org/publicdomain/zero/1.0/
+ * Just in case anyone wants to use this, being clear that it's the same license as Metrics itself:
  *
- * http://gee.cs.oswego.edu/cgi-bin/viewcvs.cgi/jsr166/src/jsr166e/LongAdder.java?revision=1.14&view=markup
+ * Copyright 2015 PaperCut Software International Pty. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
-
 package com.codahale.metrics;
 
 import java.io.Serializable;
 import java.util.concurrent.atomic.AtomicLong;
 
-// CHECKSTYLE:OFF
 /**
- * One or more variables that together maintain an initially zero {@code long} sum.  When updates
- * (method {@link #add}) are contended across threads, the set of variables may grow dynamically to
- * reduce contention. Method {@link #sum} (or, equivalently, {@link #longValue}) returns the current
- * total combined across the variables maintaining the sum.
- * <p/>
- * <p>This class is usually preferable to {@link AtomicLong} when multiple threads update a common
- * sum that is used for purposes such as collecting statistics, not for fine-grained synchronization
- * control.  Under low update contention, the two classes have similar characteristics. But under
- * high contention, expected throughput of this class is significantly higher, at the expense of
- * higher space consumption.
- * <p/>
- * <p>This class extends {@link Number}, but does <em>not</em> define methods such as {@code
- * equals}, {@code hashCode} and {@code compareTo} because instances are expected to be mutated, and
- * so are not useful as collection keys.
- * <p/>
- * <p><em>jsr166e note: This class is targeted to be placed in java.util.concurrent.atomic.</em>
+ * The
+ * <a href="https://github.com/dropwizard/metrics/blob/master/metrics-core/src/main/java/com/codahale/metrics/LongAdder.java">
+ * stock Metrics version of LongAdder
+ * </a>
+ * makes use of an App Engine blacklisted class (sun.misc.Unsafe). This class replaces method implementations with
+ * AtomicLong equivalents. The result is a class that will run in App Engine, but has more contention (within an
+ * instance) (and a bit less memory overhead).
+ *<p>
+ * Supposedly Striped64 will be in Java 8, so if App Engine ever update there will be no need for this hack.
  *
- * @author Doug Lea
- * @since 1.8
+ * @see <a href="https://github.com/dropwizard/metrics/issues/539">Issue asking for removal of Unsafe</a>
  */
-@SuppressWarnings("all")
-class LongAdder extends Striped64 implements Serializable {
+class LongAdder extends AtomicLong implements Serializable {
     private static final long serialVersionUID = 7249069246863182397L;
 
-    /**
-     * Version of plus for use in retryUpdate
-     */
-    final long fn(long v, long x) {
-        return v + x;
-    }
-
-    /**
-     * Creates a new adder with initial sum of zero.
-     */
     LongAdder() {
     }
 
-    /**
-     * Adds the given value.
-     *
-     * @param x the value to add
-     */
     public void add(long x) {
-        Cell[] as;
-        long b, v;
-        HashCode hc;
-        Cell a;
-        int n;
-        if ((as = cells) != null || !casBase(b = base, b + x)) {
-            boolean uncontended = true;
-            int h = (hc = threadHashCode.get()).code;
-            if (as == null || (n = as.length) < 1 ||
-                    (a = as[(n - 1) & h]) == null ||
-                    !(uncontended = a.cas(v = a.value, v + x)))
-                retryUpdate(x, hc, uncontended);
-        }
+        addAndGet(x);
     }
 
-    /**
-     * Equivalent to {@code add(1)}.
-     */
     public void increment() {
-        add(1L);
+        incrementAndGet();
     }
 
-    /**
-     * Equivalent to {@code add(-1)}.
-     */
     public void decrement() {
-        add(-1L);
+        decrementAndGet();
     }
 
-    /**
-     * Returns the current sum.  The returned value is <em>NOT</em> an atomic snapshot; invocation
-     * in the absence of concurrent updates returns an accurate result, but concurrent updates that
-     * occur while the sum is being calculated might not be incorporated.
-     *
-     * @return the sum
-     */
     public long sum() {
-        long sum = base;
-        Cell[] as = cells;
-        if (as != null) {
-            int n = as.length;
-            for (int i = 0; i < n; ++i) {
-                Cell a = as[i];
-                if (a != null)
-                    sum += a.value;
-            }
-        }
-        return sum;
+        return get();
     }
 
-    /**
-     * Resets variables maintaining the sum to zero.  This method may be a useful alternative to
-     * creating a new adder, but is only effective if there are no concurrent updates.  Because this
-     * method is intrinsically racy, it should only be used when it is known that no threads are
-     * concurrently updating.
-     */
     public void reset() {
-        internalReset(0L);
+        set(0);
     }
 
-    /**
-     * Equivalent in effect to {@link #sum} followed by {@link #reset}. This method may apply for
-     * example during quiescent points between multithreaded computations.  If there are updates
-     * concurrent with this method, the returned value is <em>not</em> guaranteed to be the final
-     * value occurring before the reset.
-     *
-     * @return the sum
-     */
     public long sumThenReset() {
-        long sum = base;
-        Cell[] as = cells;
-        base = 0L;
-        if (as != null) {
-            int n = as.length;
-            for (int i = 0; i < n; ++i) {
-                Cell a = as[i];
-                if (a != null) {
-                    sum += a.value;
-                    a.value = 0L;
-                }
-            }
-        }
-        return sum;
-    }
-
-    /**
-     * Returns the String representation of the {@link #sum}.
-     *
-     * @return the String representation of the {@link #sum}
-     */
-    public String toString() {
-        return Long.toString(sum());
-    }
-
-    /**
-     * Equivalent to {@link #sum}.
-     *
-     * @return the sum
-     */
-    public long longValue() {
-        return sum();
-    }
-
-    /**
-     * Returns the {@link #sum} as an {@code int} after a narrowing primitive conversion.
-     */
-    public int intValue() {
-        return (int) sum();
-    }
-
-    /**
-     * Returns the {@link #sum} as a {@code float} after a widening primitive conversion.
-     */
-    public float floatValue() {
-        return (float) sum();
-    }
-
-    /**
-     * Returns the {@link #sum} as a {@code double} after a widening primitive conversion.
-     */
-    public double doubleValue() {
-        return (double) sum();
+        return getAndSet(0);
     }
 
     private void writeObject(java.io.ObjectOutputStream s)
             throws java.io.IOException {
         s.defaultWriteObject();
-        s.writeLong(sum());
+        s.writeLong(get());
     }
 
     private void readObject(java.io.ObjectInputStream s)
             throws java.io.IOException, ClassNotFoundException {
         s.defaultReadObject();
-        busy = 0;
-        cells = null;
-        base = s.readLong();
+        set(s.readLong());
     }
 }
-// CHECKSTYLE:ON
